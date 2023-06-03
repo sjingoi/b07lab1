ab83450200e8321943e59efec75434c50d2ab7c2
Sebastian Jingoi <sebi.jingoi@gmail.com>
2023-06-03 18:28:24 -0400

    Added evaluate function

--- Polynomial.java
+++ Polynomial.java
@@ -1,8 +1,10 @@
+import java.io.Console;
+
 public class Polynomial {
     double[] coefficients;
 
     public Polynomial() {
-        coefficients = new double[0];
+        coefficients = new double[1];
         coefficients[0] = 0;
     }
 
@@ -11,9 +13,6 @@ public class Polynomial {
     }
 
     public Polynomial add(Polynomial polynomial) {
-        int lengthOfSmallest;
-        int lengthOfLargest;
-
         Polynomial smallPoly;
         Polynomial largePoly;
 
@@ -25,16 +24,31 @@ public class Polynomial {
             largePoly = polynomial;
         }
 
-        Polynomial newPoly = new Polynomial;
-        int[] newCoeff = new int[largePoly.coefficients.length]
+        double[] newCoeff = new double[largePoly.coefficients.length];
 
         for (int i = 0; i < smallPoly.coefficients.length; i++) {
-            newCoeff[i] = smallPoly.coefficients[i] + largePoly.coefficients[i]
+            newCoeff[i] = smallPoly.coefficients[i] + largePoly.coefficients[i];
         } 
         for (int i = smallPoly.coefficients.length; i < largePoly.coefficients.length; i++) {
-            newCoeff[i] = newPoly.coefficients[i]
+            newCoeff[i] = largePoly.coefficients[i];
         } 
  
         return new Polynomial(newCoeff);
+    }
+
+    public double evaluate(double x) {
+        double y = 0;
+        for (int deg = 0; deg < coefficients.length; deg++) {
+            double term = coefficients[deg];
+            for (int i = 0; i < deg; i++) {
+                term = term * x;
+            }
+            y += term;
+        }
+        return y;
+    }
+
+    public boolean hasRoot(double root) {
+        return evaluate(root) == 0;
     }
 }
